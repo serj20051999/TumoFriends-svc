09323cfde0c27ee186660647fc3921f1aa60a655
Mesrob <mesrobk.dev@gmail.com>
2019-04-27 17:12:30 -0700

    add db call to create students

--- routes/students.js
+++ routes/students.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+var db = require('../db');
+
 /* Get a single student: req.user.username */
 router.get('/students/:email', /*, passport.authenticate('basic', { session: false }) */
   function(req, res, next) {
@@ -9,7 +11,7 @@ router.get('/students/:email', /*, passport.authenticate('basic', { session: fal
 });
 
 /* Create a student account */
-router.post('/students', function(req, res, next) {
+router.post('/students', async function(req, res, next) {
   const student = {
     email: req.body.email,
     firstName: req.body.firstName,
@@ -18,9 +20,21 @@ router.post('/students', function(req, res, next) {
     learningTargets: req.body.learningTargets,
     location: req.body.location
   };
-  console.log(req.db);
-  // app.get('db').createCollection("students", student);
-  res.send(student);
+  const cursor = await db.getClient().collection("students").findOne({email: student.email},
+    function(err, results) {
+      if (results) {
+        res.status(400).send({error: 'Student ' + student.email + ' already exists!'});
+      } else {
+        db.getClient().collection("students").insertOne(student, function(err, r) {
+          if (err) {
+            res.send(err);
+          }
+          else {
+            res.send(student);
+          }
+        })
+      }
+    });
 });
 
 /* Update a student account */
