def44b03dbe16daa8b0a073b5f6f690f8f0994c6
Mesrob <mesrobk.dev@gmail.com>
2019-05-04 19:47:10 -0700

    search w/ socketio

--- socket.js
+++ socket.js
@@ -19,10 +19,33 @@ function connect(server) {
 }
 
 // TODO: List namespace will provide list of logged in users
-function studentList(io) {}
+function studentList(io) {
+  const list = io.of('/list');
+  list.on('connection', socket => {
+    
+    socket.on('hello', user => {
+      // Update user db with student socket id
+
+      // Tell everyone this user logged in
+      list.emit('logged-in', user);
+    });
+
+    socket.on('query', (params) => {
+      // For a given search params, return student list
+      db.getClient().collection("students").find(query).toArray(function(err, results) {
+        if (err) {
+          socket.emit('error', err);
+        } else {
+          socket.emit('results', results);
+        }
+      });
+    });
+  });
+}
 
 // TODO: Collaboration namespace for communicating between users
 function collaboration(io) {}
+
 
 module.exports = {
   connect,
